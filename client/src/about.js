bb975f4c8584b164060aaa7862504948ff00764e
Sirina Håland <90001777+SirinaHaaland@users.noreply.github.com>
2024-05-15 22:42:07 +0200

    Update about.js

--- client/src/about.js
+++ client/src/about.js
@@ -5,7 +5,7 @@ function AboutPage() {
     return (
       <div className="about-page">
         <h1>About Mind Map</h1>
-        <p>Mind Map is a web application designed to transform the way users interact with large collections of spoken content, such as lectures, podcasts, and seminars. Using advanced Natural Language Processing and Machine Learning techniques, Mind Map categorizes spoken content into intuitive, searchable topics, allowing users to quickly find information relevant to their interests and research needs.</p>
+        <p>Mind Map is a Web application designed to transform the way users interact with large collections of spoken content, such as lectures, podcasts, and seminars. Using advanced Natural Language Processing and Machine Learning techniques, Mind Map categorizes spoken content into intuitive, searchable topics, allowing users to quickly find information relevant to their interests and research needs.</p>
         
         <h2>Design Inspiration</h2>
         <p>Our user interface is inspired by the vastness of the universe, reflecting the expansive nature of large collections of spoken content. The UI shows topics as clustered nodes of images, with smaller nodes in orbit around the main topic node, reminiscent of celestial bodies in space. These nodes represent individual talks or recordings, clickable and leading users directly to their respective audio and transcripts pages, enhancing the exploratory experience of navigating through a galaxy of ideas.</p>
@@ -18,14 +18,14 @@ function AboutPage() {
         </ul>
   
         <h2>How It Works</h2>
-        <p>At the core of Mind Map is our sophisticated topic modeling system that processes audio transcripts to identify and categorize topics using TF-idf and K-Means clustering. Users interact with a responsive and intuitive interface where they can navigate through topics and access audio and transcripts directly. Leveraging cutting-edge AI, Mind Map enhances topic identification and visual representation, making exploration both engaging and insightful.</p>
+        <p>At the core of Mind Map is our sophisticated topic modeling system that processes audio transcripts to identify and categorize topics using tF-idf and K-Means clustering. Users interact with a responsive and intuitive interface where they can navigate through topics and access audio and transcripts directly. Leveraging cutting-edge AI, Mind Map enhances topic identification and visual representation, making exploration both engaging and insightful.</p>
   
         <h2>Who We Are</h2>
         <p>Mind Map was developed by a dedicated team of computer science students, passionate about making digital education more accessible and navigable. Our team combines expertise in machine learning, software development, and user experience to create innovative solutions to complex problems.</p>
   
         <h2>Contact Us</h2>
         <p>For more information, support, or feedback, please contact us at 2024mindmap@gmail.com.</p>
-        <p>Open Source Contribution: The web application and associated custom preprocessing and image-generation scripts are available as open-source resources on GitHub <a href="https://github.com/SirinaHaaland/STG0006948">here</a>. This supports transparent collaboration and adaptation of our methodologies to new speech media collections by the community.</p>
+        <p>Open Source Contribution: The Web application and associated topic modeling, preprocessing and image-generation scripts are available as open-source resources on GitHub <a href="https://github.com/SirinaHaaland/STG0006948">here</a>. This supports transparent collaboration and adaptation of our methodologies to new speech media collections by the community.</p>
   
         <p>Explore, learn, and discover with Mind Map today!</p>
       </div>
